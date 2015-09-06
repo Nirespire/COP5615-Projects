b51bf7b07010c66ca5a20c0289d10532586f6043
Sanjay Nair <sanjayhnair@gmail.com>
2015-09-06 16:34:23 -0400

    Copied out comments. Needs more work

--- Project1/src/main/scala/project1.scala
+++ Project1/src/main/scala/project1.scala
@@ -1,7 +1,6 @@
 import akka.actor._
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
-
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
@@ -23,30 +22,26 @@ object project1 extends App {
                     | }
                     |}
                   """.stripMargin
+
   val input = args(0)
   val ipStr = "akka.remote.netty.tcp.hostname = \"" + java.net.InetAddress.getLocalHost.getHostAddress + "\""
   val config = ConfigFactory.load(ConfigFactory.parseString(configStr + ipStr))
   val appName = config.getString("app.name")
-  // constant that prefixes all bitcoins to be hashed
   val prefix = config.getString("app.prefix")
   val port = config.getInt("akka.remote.netty.tcp.port")
   val workUnit = config.getInt("app.work_unit")
   val numOfWorkers = config.getInt("app.number_of_workers")
 
-  // Changed from NUMBER_OF_COINS to WORK_UNIT, actually, work unit is not the number of strings being sent to each
-  // worker, but some indication of where that workers assigment of work starts and ends
-  // the workAssigner class needs to be changed to be a WorkAssigner
-  // This realization came one you see how the workers and workAssigner interacts, they send each other messages
-  // If workAssigner is expected to generate the string, there may be times, where other workers are idle
-  // Each worker should be given enough work without keeping the workAssigner class too busy/ too relaxed is the ideal
-  // work unit.
-
   val system = ActorSystem(name = appName, config = config)
+
+  // If args(0) is a number, start the master system
   val (workAssigner, findIndicator) = if (input.matches("^\\d+$")) {
     val k = input.toInt
     (system.actorOf(Props(new WorkAssigner(k = k)), name = "workAssigner"),
       system.actorOf(Props[FindIndicator], name = "findIndicator"))
-  } else {
+  }
+  // Else, it's an IP address, so start a remote worker system and connect to the input IP
+  else {
     //TODO: throw exception in case the master actors are not found
     implicit val timeout = Timeout(5 seconds)
     (Await.result(system.actorSelection(s"akka.tcp://$appName@$input:$port/user/workAssigner").
@@ -61,22 +56,27 @@ object project1 extends App {
       prefix = prefix, workUnit = workUnit)), name = s"worker$idx")
   }
 
+  // Run for some amount of time
   Thread.sleep(180000)
   system.shutdown()
 }
 
 object StringIterator {
+  // Using the visible range of ASCII characters up till DEL 127
   val startIdx = 32
   val stopIdx = 126
 
   @inline def startChar = startIdx.toChar
-
   @inline def startString = startChar.toString
-
   @inline def stopChar = stopIdx.toChar
-
   @inline def stopString = stopChar.toString
 
+  /*
+   * Provided any string 'a', will return the ascii equivalent of adding 1 to the last character
+   * For example (assumming a-z is your ascii range):
+   *  plus("aaa") === "aab"
+   *  plus("zzz") === "aaaa"
+   */
   def plus(a: String): String = {
     if (a.last == stopChar) {
       plus(a.substring(0, a.length - 1)) + startChar
@@ -106,16 +106,7 @@ class WorkAssigner(k: Int) extends Actor {
       println(x) //This will be the workers, ip address
     case false => sender ! k
     case true =>
-      //Instead send a seed string, and each worker will have to try say adding all combinations
-      // of three chars appended to given seed,
-      // example,if WORK_UNIT is 3 & the seed send to the worker is "a", worker is expected to try everything
-      // from snairaaaa to sanirazzz, in this way we are assigning each worker a unique set of stuff to try
-      // among themselves.
-      // The only exception is when a blank seed ("") is  sent to a worker, that work will try all
-      // 0 to 3 length string combinations , e.g. snair, snaira, sanirb,... sanirzzz.
 
-      //We need to see which work unit i.e. number of combinations(2,3,4, or more) should
-      // each worker try, and which is most efficient.
       sender ! seed
       seed = StringIterator.getNextCombo(seed)
   }
@@ -125,6 +116,7 @@ class Worker(workAssigner: ActorRef, findIndicator: ActorRef, prefix: String, wo
   var k: Int = _
   workAssigner ! false
 
+  // Bitcoin hash function
   @inline def SHA256(s: String): String = {
     val m = java.security.MessageDigest.getInstance("SHA-256").digest(s.getBytes("UTF-8"))
     m.map("%02x".format(_)).mkString
@@ -150,7 +142,7 @@ class Worker(workAssigner: ActorRef, findIndicator: ActorRef, prefix: String, wo
 class FindIndicator extends Actor {
   def receive = {
     // Print all valid bitcoin returned from Worker
-    case bitcoin: Bitcoin => println(sender + "_-_" + bitcoin)
+    case bitcoin: Bitcoin => println(/*sender + "_-_" + */bitcoin)
   }
 }
 
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # COP5615_Projects
+
+## Project 1
+
+### Structure
 <pre>
 +______________________________________________________+
 |                      _______                         |
@@ -20,3 +24,12 @@
 |                     |_______|                        |
 +______________________________________________________+
        </pre>
+
+  Work unit is not the number of strings being sent to each
+  worker, but some indication of where that workers assigment of work starts and ends
+  the workAssigner class needs to be changed to be a WorkAssigner
+  This realization came one you see how the workers and workAssigner interacts, they send each other messages
+  If workAssigner is expected to generate the string, there may be times, where other workers are idle
+  Each worker should be given enough work without keeping the workAssigner class too busy/ too relaxed is the ideal
+  work unit.
+
