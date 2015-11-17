fb6a5db946eb34516380226247ce6aae6f2d86fa
preethu19th <preethu.thomas@ufl.edu>
2015-11-17 15:44:38 -0500

    Fixing case _ issue

--- Project4/src/main/scala/Server/Actors/PageActor.scala
+++ Project4/src/main/scala/Server/Actors/PageActor.scala
@@ -5,7 +5,6 @@ import Objects.Page
 class PageActor(var page: Page) extends ProfileActor {
   def pageReceive: Receive = {
     case newPage: Page => page = newPage
-    case _ =>
   }
 
   override def receive = pageReceive orElse super.receive
--- Project4/src/main/scala/Server/Actors/UserActor.scala
+++ Project4/src/main/scala/Server/Actors/UserActor.scala
@@ -8,12 +8,11 @@ import spray.json._
 
 import scala.collection.mutable
 
-class UserActor(user: User) extends ProfileActor {
+class UserActor(var user: User) extends ProfileActor {
   val friendsMap = mutable.Map[ListType, mutable.Set[Int]]()
 
   def userReceive: Receive = {
     case rc: RequestContext => rc.complete(user)
-    case _ =>
   }
 
   override def receive = userReceive orElse super.receive
