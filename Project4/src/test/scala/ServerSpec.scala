529126de9e76da5abc4b64bd30676fd215a5303a
Sanjay Nair <snair@localhost.localdomain>
2015-11-28 11:21:01 -0500

    added server test file

---
+++ Project4/src/test/scala/ServerSpec.scala
@@ -0,0 +1,71 @@
+import Server.RootService
+import org.joda.time.DateTime
+import org.scalatest.{FreeSpec, Matchers}
+import spray.testkit.ScalatestRouteTest
+import spray.http.StatusCodes._
+import Objects._
+import Objects.ObjectJsonSupport._
+
+class ServerSpec extends FreeSpec with ScalatestRouteTest with Matchers with RootService {
+  def actorRefFactory = system
+
+  "Put User" - {
+    "when calling PUT /user" - {
+      "should return a user object" in {
+        Put("/user", User(BaseObject(), "about me", "birthday", 'M', "first name", "last name")) ~> myRoute ~> check {
+          status should equal(OK)
+          println(entity.toString())
+        }
+      }
+    }
+  }
+
+  "Put Page" - {
+    "when calling PUT /page" - {
+      "should return a page object" in {
+        Put("/page", Page(BaseObject(), "about", "category", -1)) ~> myRoute ~> check {
+          status should equal(OK)
+          println(entity.toString())
+        }
+      }
+    }
+  }
+
+  "Put Post" - {
+    "when calling PUT /post" - {
+      "should return a post object" in {
+        Put("/user", User(BaseObject(), "about me", "birthday", 'M', "first name", "last name")) ~> myRoute ~> check {
+          println(entity.toString())
+          Put("/post", Objects.Post(BaseObject(), 2, new DateTime().toString(), 2, "status", Objects.ObjectTypes.PostType.status, -1)) ~> myRoute ~> check {
+            status should equal(OK)
+            println(entity.toString())
+          }
+        }
+      }
+    }
+  }
+
+  "Put Picture" - {
+    "when calling PUT /picture" - {
+      "should return a picture object" in {
+        Put("/picture", Picture(BaseObject(),0, -1, "filename", "blah")) ~> myRoute ~> check {
+          status should equal(OK)
+          println(entity.toString())
+        }
+      }
+    }
+  }
+
+  "Put Album" - {
+    "when calling PUT /album" - {
+      "should return a album object" in {
+        Put("/album", Album(BaseObject(), 0, new DateTime().toString(), new DateTime().toString(), 0, "description")) ~> myRoute ~> check {
+          status should equal(OK)
+          println(entity.toString())
+        }
+      }
+    }
+  }
+
+
+}
