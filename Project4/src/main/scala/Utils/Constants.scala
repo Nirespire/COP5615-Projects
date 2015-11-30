cf5ad025e1eba38fd716ecb5422207aafe632a7c
preethu19th <preethu.thomas@ufl.edu>
2015-11-30 09:12:59 -0500

    started with debug stats for post and deleted

--- Project4/src/main/scala/Utils/Constants.scala
+++ Project4/src/main/scala/Utils/Constants.scala
@@ -3,18 +3,22 @@ package Utils
 import spray.json.JsNumber
 
 object Constants {
-  val putProfilesChar = 'P'
-  val putPostsChar = 'O'
-  val putAlbumsChar = 'A'
-  val putPicturesChar = 'I'
-  val postFlChar = 'F'
-  val getProfilesChar = 'p'
-  val getPostsChar = 'o'
-  val getPicturesChar = 'i'
-  val getAlbumsChar = 'a'
-  val getFlChar = 'f'
+  val postProfilesChar = '1'
+  val postPostsChar = '2'
+  val postAlbumsChar = '3'
+  val postPicturesChar = '4'
+  val putProfilesChar = '5'
+  val putPostsChar = '6'
+  val putAlbumsChar = '7'
+  val putPicturesChar = '8'
+  val postFlChar = '9'
+  val getProfilesChar = '0'
+  val getPostsChar = 'a'
+  val getPicturesChar = 'b'
+  val getAlbumsChar = 'c'
+  val getFlChar = 'd'
   val getFeedChar = 'e'
-  val likeChar = 'l'
+  val postLikeChar = 'f'
   val nano = 1000000000.0
   val trueBool = true
   val falseBool = false
