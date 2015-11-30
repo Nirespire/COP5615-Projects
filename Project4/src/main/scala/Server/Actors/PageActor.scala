6f11c04326f46f45169d2ea63a0f9f9e0b4609c4
Sanjay Nair <sanjayhnair@gmail.com>
2015-11-30 09:08:35 -0500

    Added proper server response for page and user deletes

--- Project4/src/main/scala/Server/Actors/PageActor.scala
+++ Project4/src/main/scala/Server/Actors/PageActor.scala
@@ -1,7 +1,8 @@
 package Server.Actors
 
-import Objects.Page
+import Objects.{BaseObject, Page}
 import Objects.ObjectJsonSupport._
+import Utils.Constants
 import spray.json._
 import Server.Messages._
 import akka.actor.ActorRef
@@ -11,31 +12,33 @@ class PageActor(var page: Page, debugActor: ActorRef)
 
   def baseObject = page.baseObject
 
+  val deletedPage = Page(BaseObject(deletedIdx, Constants.trueBool), "deleted", "deleted", -1)
+
   def pageReceive: Receive = {
     case cMsg@CreateMsg(rc, _, p: Page) =>
       if (baseObject.deleted) {
-        rc.complete(ResponseMessage("Page already deleted!"))
+        rc.complete(deletedPage)
       } else {
         page = p
         rc.complete(page)
       }
     case updMsg@UpdateMsg(rc, _, newPage: Page) =>
       if (baseObject.deleted) {
-        rc.complete(ResponseMessage("Page already deleted!"))
+        rc.complete(deletedPage)
       } else {
         page = newPage
         rc.complete(page)
       }
     case getMsg@GetMsg(rc, _, ("page", -1)) =>
       if (baseObject.deleted) {
-        rc.complete(ResponseMessage("Page already deleted!"))
+        rc.complete(deletedPage)
       } else {
         rc.complete(page)
       }
     case deleteMsg@DeleteMsg(rc, _, None) => baseObject.delete(rc, s"Page ${page.baseObject.id}")
     case likeMsg@LikeMsg(rc, _, fid, ("page", _)) =>
       if (baseObject.deleted) {
-        rc.complete(ResponseMessage(s"Profile $pid already deleted"))
+        rc.complete(deletedPage)
       } else {
         baseObject.appendLike(fid)
         rc.complete(ResponseMessage("Page liked"))
--- Project4/src/main/scala/Server/Actors/UserActor.scala
+++ Project4/src/main/scala/Server/Actors/UserActor.scala
@@ -3,8 +3,9 @@ package Server.Actors
 import Objects.ObjectJsonSupport._
 import Objects.ObjectTypes.ListType
 import Objects.ObjectTypes.ListType
-import Objects.{UpdateFriendList, User}
+import Objects.{BaseObject, UpdateFriendList, User}
 import Server.Messages._
+import Utils.Constants
 import akka.actor.ActorRef
 import spray.json._
 
@@ -15,26 +16,28 @@ class UserActor(var user: User, debugActor: ActorRef)
 
   val friendsMap = mutable.Map[ListType.Value, Set[Int]]()
 
+  val deletedUser = User(BaseObject(deletedIdx, Constants.trueBool), "deleted", "00-00-0000", 'M', "deleted", "deleted")
+
   def baseObject = user.baseObject
 
   def userReceive: Receive = {
     case cMsg@CreateMsg(rc, _, u: User) =>
       if (baseObject.deleted) {
-        rc.complete(ResponseMessage("User already deleted!"))
+        rc.complete(deletedUser)
       } else {
         user = u
         rc.complete(user)
       }
     case updMsg@UpdateMsg(rc, _, newUser: User) =>
       if (baseObject.deleted) {
-        rc.complete(ResponseMessage("User already deleted!"))
+        rc.complete(deletedUser)
       } else {
         user = newUser
         rc.complete(user)
       }
     case updMsg@UpdateMsg(rc, _, upd: UpdateFriendList) =>
       if (baseObject.deleted) {
-        rc.complete(ResponseMessage("User already deleted!"))
+        rc.complete(deletedUser)
       } else {
         user.baseObject.appendLike(upd.fid)
         if (friendsMap.contains(upd.listType)) {
@@ -46,13 +49,13 @@ class UserActor(var user: User, debugActor: ActorRef)
       }
     case getMsg@GetMsg(rc, _, ("user", -1)) =>
       if (baseObject.deleted) {
-        rc.complete(ResponseMessage("User already deleted!"))
+        rc.complete(deletedUser)
       } else {
         rc.complete(user)
       }
     case getMsg@GetMsg(rc, _, ("friendlist", listTypeIdx: Int)) =>
       if (baseObject.deleted) {
-        rc.complete(ResponseMessage("User already deleted!"))
+        rc.complete(deletedUser)
       } else {
         val listType = ListType(listTypeIdx)
         rc.complete(JsArray(friendsMap.getOrElse(listType, Set()).map(f => JsNumber(f)).toVector))
